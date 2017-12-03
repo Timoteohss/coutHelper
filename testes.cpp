78dc26eb51f1454d2c421ea2015429c6d225b7fb
Timoteo <timoteohss@gmail.com>
2017-12-03 14:02:42 +0000

    Testes finalizados

--- testes.cpp
+++ testes.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <unistd.h>
 #include "coutHelper.h"
 
 using namespace std;
@@ -9,9 +10,16 @@ using cHe::Fundo;
 
 int main() {
 	cHe::cHe impressora;
-	impressora.setBEsq("Carregando:", Frente::Preto, Fundo::Branco);
+	impressora.setBEsq("Carregando[", Frente::Preto, Fundo::Branco);
+	impressora.setBDir("]",Frente::Preto, Fundo::Branco);
 
-	cout << impressora << endl;
+	//cout << impressora << endl;
+
+	for(int x = 0; x < (impressora.larMax() -12); x++) {
+		impressora.setBPonto("#", Frente::Preto, Fundo::Branco, 11+x);
+		usleep(10000);
+		cout << impressora;
+	}
 
 	return 0;
 }
